77cb075baf6b813e4709fcd893b1e428f244c605
TKtiki <44247218+TKtiki@users.noreply.github.com>
2025-08-14 09:07:14 +0200

    test 11

--- src/main/java/biz/paluch/logging/gelf/DynamicMdcMessageField.java
+++ src/main/java/biz/paluch/logging/gelf/DynamicMdcMessageField.java
@@ -6,7 +6,8 @@ import java.util.regex.Pattern;
  * @author Mark Paluch
  * @since 28.02.14 09:56
  */
-public class DynamicMdcMessageField implements MessageField { private String regex;
+public class DynamicMdcMessageField implements MessageField {
+    private String regex;
     private Pattern pattern;
 
     public DynamicMdcMessageField(String regex) {
