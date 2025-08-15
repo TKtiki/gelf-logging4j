9b822e77427182471f8719e58de2c01efe8d8ef4
TKtiki <44247218+TKtiki@users.noreply.github.com>
2025-08-15 23:20:48 +0200

    fix

--- src/test/java/biz/paluch/logging/gelf/intern/sender/GelfTCPSenderIntegrationTests.java
+++ src/test/java/biz/paluch/logging/gelf/intern/sender/GelfTCPSenderIntegrationTests.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
